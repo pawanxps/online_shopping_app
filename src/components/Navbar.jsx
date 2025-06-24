cbe51cc3a3fcf529732656e155be8bc6ae9f82a2
Pawan Kumar <140512043+pawanxps@users.noreply.github.com>
2025-06-24 15:38:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export function Navbar() {
             <Container className="d-flex justify-content-between align-items-center">
                 <NavLink to="/" className="navbar-brand">
                     <img src="/imgs/logo.svg" alt="Logo" width="30" height="30" />PNET 
-                    <span className="text-primary text-color">Cloud Technologies</span>
+                    <span className="text-primary text-color">DevOps</span>
                 </NavLink>
 
                 <div className="d-flex align-items-center gap-3">
